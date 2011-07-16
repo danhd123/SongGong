01e07168e96132885f75307d948c1ea6e621132d
Arshad Tayyeb <developer@snarshad.com>
2011-07-15 23:31:42 -0700

    Making it iOS 4.3 compliant

--- main.m
+++ main.m
@@ -12,7 +12,8 @@
 
 int main(int argc, char *argv[])
 {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SongGongAppDelegate class]));
-    }
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([SongGongAppDelegate class]));
+    [pool release];
+    return retVal;
 }
